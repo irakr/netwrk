8c35f45fd0f84395e7ce0c5c6349ce4110b7e6aa
Irak Rigia <tarakrigia@gmail.com>
2021-12-25 00:38:10 +0530

    Makefile support for multiple platforms.

---
+++ tools/ftp-cli/raspi.mk
@@ -0,0 +1,15 @@
+TOOLCHAIN_PREFIX = arm-linux-gnueabihf-
+
+PLATFORM_INCLUDES = -I$(SYSROOT_DIR)/usr/include/arm-linux-gnueabihf
+
+# Library linkages
+PLATFORM_LIBS = -L$(SYSROOT_DIR)/usr/lib/arm-linux-gnueabihf 	\
+	-L$(SYSROOT_DIR)/opt/vc/lib -L$(SYSROOT_DIR)/usr/local/lib					\
+	-Wl,--start-group \
+	-lpthread -ldl	\
+	-Wl,--end-group
+
+PLATFORM_LFLAGS = -Wl,-rpath-link,$(SYSROOT_DIR)/opt/vc/lib \
+	-Wl,-rpath-link,$(SYSROOT_DIR)/usr/lib/arm-linux-gnueabihf \
+	-Wl,-rpath-link,$(SYSROOT_DIR)/lib/arm-linux-gnueabihf
+
